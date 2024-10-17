b7752a84e5fc9ae31ae1f9293a88732c03bda4fe
chowcz222 <chowchengzhe@gmail.com>
2024-10-17 10:41:14 +0800

    added MyImplementation

---
+++ src/appdata/MyImplementation.java
@@ -0,0 +1,18 @@
+package appdata;
+
+public class MyImplementation implements Runnable {
+
+    @Override
+    public void run() {
+
+        RunningThreadCount();
+
+    }
+
+    public void RunningThreadCount() {
+
+        for(int i = 0; i < 10; i++) {
+            System.out.println(Thread.currentThread().getName() + " -> " +i);
+        }
+    }
+}
