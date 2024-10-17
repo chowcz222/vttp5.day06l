3ad9e0dee51f082713b6daacafb2071757c1b89f
chowcz222 <chowchengzhe@gmail.com>
2024-10-17 10:12:35 +0800

    initial commmit

---
+++ src/appdata/App.java
@@ -0,0 +1,18 @@
+package appdata;
+
+public class App {
+
+    public static void main(String[] args) {
+        
+        Thread th = new Thread(new Runnable() {
+
+            public void run() {
+
+                for(int i = 0; i < 10; i++) {
+                    System.out.println(Thread.currentThread().getName() + " -> " +i);
+                }
+            }    
+        });
+        th.start();
+    }
+}
